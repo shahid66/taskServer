245a7f76d77c4d7025b82671bb6fe3b884e5b16b
shahid66 <kk.shahid66@gmail.com>
2023-08-23 00:51:22 +0600

    cors update2

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://taskmanager.kkshahid.com",
+  })
+);
 
 // DB Connection
 mongoose
