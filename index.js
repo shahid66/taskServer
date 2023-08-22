15f7e5411a1707dbd66cd35ad56b42dd0847b708
shahid66 <kk.shahid66@gmail.com>
2023-08-23 00:49:33 +0600

    cors update

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 // DB Connection
 mongoose
